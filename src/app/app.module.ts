634bd5630890389d4c33e585d95abff3b7eb5a14
Nima Baghdadi <bg.nima@gmail.com>
2019-08-13 22:39:18 +0200

    initial page structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { TopMenuComponent } from './components/top-menu/top-menu.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { PreviewAreaComponent } from './components/preview-area/preview-area.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMenuComponent,
+    SideBarComponent,
+    PreviewAreaComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
